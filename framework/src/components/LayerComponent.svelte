77f0ed279c863e4761af77909eea7c94b6d8f834
elproffesore <philipp.proff@gmx.de>
2024-12-04 17:08:33 +0100

    master update

--- framework/src/components/LayerComponent.svelte
+++ framework/src/components/LayerComponent.svelte
@@ -17,9 +17,7 @@
             if (range.startContainer != range.endContainer  == "SPAN") return;
             let span = document.createElement("span");
             span.style.textDecoration = "underline";
-            span.style.textDecorationColor = "#ff0000bb";
-            span.style.textDecorationThickness = "1.5px";
-            span.style.textUnderlineOffset = "1px";
+            span.style.backgroundColor = "#ff000055";
             if (
                 selection.anchorOffset != selection.focusOffset &&
                 selection.getRangeAt(0).toString().trim() != ""
@@ -33,7 +31,7 @@
 </script>
     <div
         id="layer{index}"   
-        class="layers absolute text-invisible border rounded-md py-4 px-8 my-8 {active?"z-10  opacity-100":"z-0 opacity-0"}"
+        class="layers absolute text-invisible py-4 px-8 my-8 {active?"z-10  opacity-100":"z-0 opacity-0"}"
     >
         <input type="text" class="border-b w-full mb-4 text-xl text-black font-bold" bind:value={iteration.name} />
         <div bind:this={layerText} class="text"></div>
