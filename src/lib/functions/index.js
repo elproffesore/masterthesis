da087b73c300d413d69b3b03b3e32c082aabfac7
elproffesore <philipp.proff@gmx.de>
2024-12-29 14:13:38 +0100

    move functions and stores into index files

--- src/lib/functions/index.js
+++ src/lib/functions/index.js
@@ -1,1 +1,34 @@
 // place files you want to import through the `$lib` alias in this folder.
+export const posColors = {
+    "Noun": "red",
+    "Verb": "blue",
+    "Adjective": "green",
+    "Adverb": "purple",
+    "Preposition": "orange",
+    "Conjunction": "lime",
+    "Determiner": "pink",
+     "Pronoun": "brown",
+}
+export const posTagBannlist = [
+    "Pronoun",
+];
+export function nameShortener(string){
+    if(string.length>20){
+        return string.substring(0,20)+"...";
+    }
+    return string;
+}
+export function getMostRightNode(nodes) {
+    let mostRightNode = nodes[0];
+    let maxRight = nodes[0].getBoundingClientRect().right;
+
+    nodes.forEach((node) => {
+        let right = node.getBoundingClientRect().right;
+        if (right > maxRight) {
+            maxRight = right;
+            mostRightNode = node;
+        }
+    });
+
+    return mostRightNode;
+}
---
+++ src/lib/stores/index.js
@@ -0,0 +1,3 @@
+import { writable } from "svelte/store";
+export let texts = writable([]);
+export let relations = writable([]);
