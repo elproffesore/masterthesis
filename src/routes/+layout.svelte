53535bee83938d1dd8209748e1c795b56f76554b
elproffesore <philipp.proff@gmx.de>
2024-12-29 14:14:21 +0100

    remove padding for better compatability with canvas

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.css';
 </script>
 <div class="w-full h-[50px] fixed z-10 bg-white" style="box-shadow: 0px 25px 15px #ffffff;"></div>
-<div class="py-[100px]"><slot/></div>
+<slot/>
 <div class="w-full h-[50px] fixed bottom-0 z-10 bg-white" style="box-shadow: 0px -25px 15px #ffffff;"></div>
 
 
